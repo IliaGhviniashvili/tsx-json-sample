ebf7daf082441964cb0b432c7508b77e3639b901
Ilia Ghviniashvili <ili.ghviniashvili@gmail.com>
2023-05-05 21:26:48 +0400

    tsx-json

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ProjectsPage from './projects/projectsPage'; 
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />  
+    </div>
+
+  );
+}
+
+export default App;
